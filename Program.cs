4949872d4f4e2d2fbaf555f6576d28e2540afc4f
ozgunt <117322408+ozgunt@users.noreply.github.com>
2022-11-09 16:31:52 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int toplam=6
+for(int i =basla 
+    else if (basla==bitis)
+    consele writeline(bitis )
